8e676b189d2815462e5bf717a0fe7049720cbd17
Erlend Hollund <erlend.hollund29@gmail.com>
2022-02-16 21:17:40 +0100

    Add files via upload

---
+++ RPG-Assignment/Characters/Mage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Assignment.Characters
+{
+   public class Mage : Hero{
+
+
+        // On mage create set lv and attributes
+        public Mage()
+        {
+            
+            Strength = 1;
+            Dexterity = 1;
+            Intelligence = 8;
+            PrimaryAttribute = "Intelligence";
+
+            // Sets level up stats
+            StrengthLevlUp = 1;
+            DexterityLevelUp = 1;
+            IntelligenceLevelUp = 5;
+
+            // Allows weapons
+
+            weaponAllow.Add(4);
+            weaponAllow.Add(6);
+
+            // Allows armos
+            armorAllow.Add(0);
+        }
+
+
+
+    }
+}
---
+++ RPG-Assignment/Items/Armor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Assignment.Items
+{
+    public class Armor : Item
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+
+        public int armorType {get; set;}
+
+
+        // Armor list
+        List<String> armorTypes = new List<string>()
+        {
+            "Cloth",
+            "Leather",
+            "Mail",
+            "Plate"
+        };
+
+
+        public Armor(int strength, int dexterity, int intelligence)
+        {
+            Strength = strength;
+            Dexterity = dexterity;
+            Intelligence = intelligence;
+        }
+
+        // Returns name based on what type(int) it has
+        public string getArmortypeName()
+        {
+            return armorTypes[armorType];
+        }
+    }
+}
